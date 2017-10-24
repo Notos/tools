7ef7bd7f9e502189739d20cac571150c38e90fb2
Notos Korsan <notos.korsan@gmail.com>
2017-10-24 13:52:03 -0200

    Use laravel mix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,55 +6,42 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="icon" href="/favicon.ico">
 
         <title>Notos Tools</title>
 
         <!-- Bootstrap core CSS -->
-        <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- Custom styles for this template -->
-        <link href="starter-template.css" rel="stylesheet">
-
-        <style>
-            body {
-                padding-top: 5rem;
-            }
-            .starter-template {
-                padding: 3rem 1.5rem;
-                text-align: center;
-            }
-        </style>
+        <link href="/css/app.css" rel="stylesheet">
     </head>
 
     <body>
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="#">Notos Tools</a>
-        </nav>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+                <a class="navbar-brand" href="#">Notos Tools</a>
+            </nav>
 
-        <main role="main" class="container">
+            <main role="main" class="container">
 
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
 
-            @yield('contents')
+                @yield('contents')
 
-        </main><!-- /.container -->
+            </main><!-- /.container -->
+        </div>
 
+        <script src="/js/app.js"></script>
 
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-        <script src="http://getbootstrap.com/assets/js/vendor/popper.min.js"></script>
-        <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+        @if(config('app.env') == 'local')
+            <script src="http://localhost:35729/livereload.js"></script>
+        @endif
     </body>
 </html>
